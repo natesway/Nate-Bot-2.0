9708560d6791da5df7e5041abbeed155100f1ef6
natesway <64743334+natesway@users.noreply.github.com>
2020-07-28 19:59:27 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const bot = new Discord.Client();
 
-const PREFIX = '?';
+const PREFIX = '~';
 
 bot.on("ready", function (){
      console.log('S[bot.user.username] Is Online!');
